6f2404022f9b8f3bdbdc32f64dff42f6aec801eb
linhnv <linhnv2@tinhvan.com>
2024-05-19 08:38:13 +0700

    feat: Edit and design layouts

--- Film/src/main/java/org/example/film/controllers/HomeController.java
+++ Film/src/main/java/org/example/film/controllers/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("")
     public String index(){
-        return "public/index";
+        return "public/home";
     }
 
 }
